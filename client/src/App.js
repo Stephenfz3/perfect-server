edeea91e8b34bd79af01ca0226575ff01c6b4928
Francisco Gonzalez <franciscogonzalez@Franciscos-MacBook-Pro.local>
2019-11-06 16:16:39 -0500

    added components folder, pages and modified package.jason file to include packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
+// Pacakge Imports
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// Styling Imports
 import "./App.css";
+// Component Imports
+import Nav from "./components/Nav";
+// Pages Imports
+import Dashboard from "./pages/Dashboard";
+import NoMatch from "./pages/NoMatch";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
+    </Router>
     );
   }
 }
 
 export default App;
+
