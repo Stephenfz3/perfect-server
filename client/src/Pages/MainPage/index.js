0d09066cd8580b46e1f6ab33a1850c321d273b06
Georgi <georgi.nikolow@gmail.com>
2019-11-15 14:44:40 -0500

    buttons for next and prev page work until we have 4 items in the selectedState

--- client/src/Pages/MainPage/index.js
+++ client/src/Pages/MainPage/index.js
@@ -5,12 +5,7 @@ class Detail extends Component {
 
   state = {
     current: 1,
-    cart: [{
-      apetizers: "",
-      desserts: "",
-      mainCourses: "",
-      drinks: ""
-    }]
+   selectedSteps: []
 
   }
 
@@ -26,19 +21,40 @@ class Detail extends Component {
 
 
   async componentDidMount() {
-
     let newState = this.props.location.state;
     this.setState({ ...newState })
+    let selectedSteps = this.state.selectedSteps
+
+    if(selectedSteps.length < 4 || selectedSteps.includes(!1)){
+  try {
+    this.state.apetizers.map(main => (
+      this.outside(main)
+    ))
+  } catch (err) {
+    console.log(err)
+  }
+} else {
+  console.log("CASE 1 ELSE MSG")
+}
   }
 
   nextStep = () => {
     const { current } = this.state;
-    this.setState({ current: current + 1 })
-  };
+    let {selectedSteps} = this.state;
+     if(selectedSteps.includes(current+1) || selectedSteps.length!==3) {
+      this.setState({ current: current + 1, selectedSteps: [...this.state.selectedSteps, current] })
 
+    } else if (selectedSteps.includes(current+1) && selectedSteps.length!==3 ) {
+    this.setState({ current: current + 2, selectedSteps: [...this.state.selectedSteps, current] })
+  } else {
+    console.log("YOU fiLLED YOUR CART")
+  }
+  }
   prevStep = () => {
     const { current } = this.state;
-    this.setState({ current: current - 1 });
+    const {selectedSteps} = this.state;
+     const reducedSteps = selectedSteps.filter(item => {return item !== current-1})
+    this.setState({ current: current - 1, selectedSteps: reducedSteps  });
   }
 
   outside = (id) => {
@@ -51,57 +67,77 @@ class Detail extends Component {
 
 
   render() {
-    const { current } = this.state;
+    let { current } = this.state;
+    const {selectedSteps} = this.state;
+
     switch (current) {
       case 1:
+        if(selectedSteps.length < 3 || selectedSteps.includes(!1)){
         try {
-          this.state.apetizers.map(app => (
-            this.outside(app)
-          ))
-        } catch (err) {
-          console.log(err)
-        }
-        break;
-
-      case 2:
-        try {
-          this.state.mainCourses.map(main => (
+          this.state.apetizers.map(main => (
             this.outside(main)
           ))
         } catch (err) {
           console.log(err)
-        };
+        }
+      } else {
+        console.log("CASE 1 ELSE MSG")
+      }
         break;
 
+      case 2:
+          if(selectedSteps.length<3 || selectedSteps.includes(!3)){
+            try {
+              this.state.mainCourses.map(main => (
+                this.outside(main)
+              ))
+            } catch (err) {
+              console.log(err)
+            }
+          } else {
+            console.log("CASE 2 ELSE MSG")
+          }
+            break;
+
       case 3:
+          if(selectedSteps.length<3 || selectedSteps.includes(!3)){
         try {
-          this.state.desserts.map(dessert => (
-            this.outside(dessert)
+          this.state.desserts.map(main => (
+            this.outside(main)
           ))
         } catch (err) {
-          console.log("Dessert", err)
-        };
+          console.log(err)
+        }
+      } else {
+        console.log("CASE 3 ELSE MSG")
+      }
         break;
 
       case 4:
-        try {
-          this.state.drinks.map(drink => (
-            this.outside(drink)
-          ))
-        } catch (err) {
-          console.log("Drink", err)
-        };
-        break;
+          if(selectedSteps.length<3 || selectedSteps.includes(!3)){
+            console.log("CASE 4 ELSE MSG")
+          } else {
+            try {
+              this.state.drinks.map(main => (
+                this.outside(main)
+              ))
+            } catch (err) {
+              console.log(err)
+            }
+         
+          }
+            break;
 
 
       default:
-          try {
-            this.state.apetizers.map(app => (
-              this.outside(app)
-            ))
-          } catch (err) {
-            console.log(err)
-          }
+          current = 1
+          // try {
+          //   this.state.apetizers.map(app => (
+          //     this.outside(app)
+          //   ))
+          // } catch (err) {
+          //   console.log(err)
+          // }
           break;
     }
 
@@ -112,9 +148,11 @@ class Detail extends Component {
       <div>
         <h1>MainPage</h1>
         <h2> {console.log("Main Page", this.state)}</h2>
+        {/* <div><h1>{this.state.item.map((item,index)=>(<span key={index} {...item}/>))}</h1></div> */}
         <button onClick={this.nextStep}>NExt Step</button>
+        <button onClick={this.prevStep}>NExt Step</button>
 
-
+          
 
       </div>
     );
