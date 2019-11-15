b37acbaec8fca17748ed0ef64b3c9a005dac50a3
Georgi <georgi.nikolow@gmail.com>
2019-11-15 09:46:40 -0500

    switch for next steps on MainPage works, default case set to apps/1

--- client/src/Pages/MainPage/index.js
+++ client/src/Pages/MainPage/index.js
@@ -1,59 +1,124 @@
 import React, { Component } from 'react';
 import API from "../../utils/API";
 class Detail extends Component {
-  
-  
-state= {
-  
-}
 
 
-// async componentDidMount() {
-//   try {
-//     const response = await fetch(`/api/foodpage/:id`);
-//     const json = await response.json();
-//     this.setState({ data: json });
-//   } catch (error) {
-//     console.log(error);
-//   }
+  state = {
+    current: 1,
+    cart: [{
+      apetizers: "",
+      desserts: "",
+      mainCourses: "",
+      drinks: ""
+    }]
+
+  }
 
 
-    async componentDidMount() {
-      try 
-      {
-        let newState  = await this.props.location.state;
-        this.setState({...newState, current: "Apps"})
-        await this.state.desserts.map( app =>  (
-             this.outside(app)
-        ))
-      } catch(err) {
+  // async componentDidMount() {
+  //   try {
+  //     const response = await fetch(`/api/foodpage/:id`);
+  //     const json = await response.json();
+  //     this.setState({ data: json });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+
+
+  async componentDidMount() {
+
+    let newState = this.props.location.state;
+    this.setState({ ...newState })
+  }
+
+  nextStep = () => {
+    const { current } = this.state;
+    this.setState({ current: current + 1 })
+  };
+
+  prevStep = () => {
+    const { current } = this.state;
+    this.setState({ current: current - 1 });
+  }
+
+  outside = (id) => {
+    API.getDetails(id)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err))
+  }
+
+
+
+
+  render() {
+    const { current } = this.state;
+    switch (current) {
+      case 1:
+        try {
+          this.state.apetizers.map(app => (
+            this.outside(app)
+          ))
+        } catch (err) {
           console.log(err)
         }
+        break;
 
-      }
-      
+      case 2:
+        try {
+          this.state.mainCourses.map(main => (
+            this.outside(main)
+          ))
+        } catch (err) {
+          console.log(err)
+        };
+        break;
 
-      outside = (id) => {
-        API.getDetails(id)
-        .then(  res => console.log(res.data))
-        .catch(err=>console.log(err))
-      }
+      case 3:
+        try {
+          this.state.desserts.map(dessert => (
+            this.outside(dessert)
+          ))
+        } catch (err) {
+          console.log("Dessert", err)
+        };
+        break;
 
-    
-    render() 
-    
-    { 
-        return ( 
-             
-            <div>
-                <h1>MainPage</h1>
-                <h2> {console.log("Main Page",this.state)}</h2>
+      case 4:
+        try {
+          this.state.drinks.map(drink => (
+            this.outside(drink)
+          ))
+        } catch (err) {
+          console.log("Drink", err)
+        };
+        break;
 
-            
 
-            </div>
-         );
+      default:
+          try {
+            this.state.apetizers.map(app => (
+              this.outside(app)
+            ))
+          } catch (err) {
+            console.log(err)
+          }
+          break;
     }
+
+
+
+    return (
+
+      <div>
+        <h1>MainPage</h1>
+        <h2> {console.log("Main Page", this.state)}</h2>
+        <button onClick={this.nextStep}>NExt Step</button>
+
+
+
+      </div>
+    );
+  }
 }
- 
+
 export default Detail;
