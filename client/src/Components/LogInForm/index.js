e42dab04800971dcb4c16833e16a72c6def7ac7d
Stephen <stephenf3@yahoo.com>
2019-11-20 22:57:55 -0500

    changes

--- client/src/Components/LogInForm/index.js
+++ client/src/Components/LogInForm/index.js
@@ -2,7 +2,7 @@
 import React, {Component} from 'react';
 import Api from '../../utils/API';
 import {Link} from 'react-router-dom';
-import '../LoginForm/loginform.css'
+import './loginform.css'
 import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput, MDBBtn, MDBIcon } from 'mdbreact';
 
 
